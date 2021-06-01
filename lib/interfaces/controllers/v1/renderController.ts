93deb39bf1258175ec5eaa4ee8ccedccce07d52a
Ángel D Barrios <angeldbarriosg11@gmail.com>
2021-06-01 09:54:56 -0500

    some cleanup

--- lib/interfaces/controllers/v1/renderController.ts
+++ lib/interfaces/controllers/v1/renderController.ts
@@ -3,11 +3,11 @@ import express, { NextFunction, Request, Response } from 'express';
 
 export default (): express.Router => {
   const router = express.Router();
-  
-  /** It makes more sense this file is not here, elsewhere in controller folder 
+
+  /** It makes more sense this file is not here, elsewhere in controller folder
    * WE ARE INSIDE v1 FOLDER
    * TODO: take this file out
-  */
+   */
   router.get('/', (_req: Request, res: Response, next: NextFunction) => {
     try {
       // Rendering React app HTML
